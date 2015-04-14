3dbf9283d282e46c0199955bf8982eafcfda34c5
huneng <huneng1991@163.com>
2015-04-14 22:10:38 +0800

    delete repeat files

--- getExtension.m
+++
@@ -1,9 +0,0 @@
-function [extension, index] = getExtension(filename)
-m_len = length(filename);
-for i=m_len: -1: 1
-    if(filename(i)=='.')
-        extension = filename(i+1:m_len);
-        index = i;
-        break;
-    end
-end
--- getairlight.m
+++
@@ -1,9 +0,0 @@
-function A = getairlight(dark)
-[h, w] = size(dark);
-A = zeros(h, w, 3);
-darkT = reshape(dark, 1, h*w);
-darkT = sort(darkT, 'descend');
-
-A = darkT(1);
-            
-            
